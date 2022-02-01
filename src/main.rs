db85bf60a2a75eb9e144db1a59d16a59f4628e8f
Angel Cervera Roldan <angelcerveraroldan765@gmail.com>
2022-02-01 13:20:10 +0000

    Commit

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let path: String = String::from(&args[1]);
+
+    // If no width is entered, use the terminals width as a default
     let desired_width = args[2]
         .parse::<u32>()
         .expect("Please make sure that that you have entered a valid resolution");
@@ -48,7 +50,7 @@ fn image_to_ascii(img: image::DynamicImage, desired_width: u32) {
         for row in 0..img_width as usize {
             let i = (column * img_width as usize) + row;
 
-            let c: char = rgb_to_ascii(
+            let pixel_as_char: char = rgb_to_ascii(
                 img_bytes[i * 3],
                 img_bytes[(i * 3) + 1],
                 img_bytes[(i * 3) + 2],
@@ -58,28 +60,11 @@ fn image_to_ascii(img: image::DynamicImage, desired_width: u32) {
             print!(
                 "{}{}{}",
                 termion::cursor::Goto((2 * row as u16) + 1, column as u16 + 1),
-                c,
-                c
+                pixel_as_char,
+                pixel_as_char
             );
         }
     }
-
-    // for i in 0..(pixel_count / 3) {
-    //     // img contains a vec<u8> that has the first pixels r, then the first pixels g, then the first pixels b, then the second pixels...
-    //     let c: char = rgb_to_ascii(
-    //         img_bytes[i * 3],
-    //         img_bytes[(i * 3) + 1],
-    //         img_bytes[(i * 3) + 2],
-    //     );
-
-    //     // Print character twice to make image look nicer
-    //     print!("{}{}", c, c);
-
-    //     if (i + 1) % desired_width as usize == 0 {
-    //         println!();
-    //     }
-    // }
-    // println!();
 }
 
 /// Given an rgb value, return an ascii character.
@@ -90,23 +75,25 @@ fn image_to_ascii(img: image::DynamicImage, desired_width: u32) {
 /// //  This is the brightest possible character to be returned.
 /// assert_eq!(rgb_to_ascii(255, 255, 255), '@');
 /// ```
-/// The charactes that can be returned are, in order from darkest to lightest: ``` .:-=+*#%@```
+/// The character that can be returned are, in order from darkest to lightest: ``` .:-=+*#%@```
 fn rgb_to_ascii(red: u8, green: u8, blue: u8) -> char {
-    // 10 ascii characters ordered from lesat area to most area
+    // 10 ascii characters ordered from least area to most area
     // In a terminal in light mode, this is ordered from lightest to darkest, but its the opposite for dark mode terminals
-    let characters = " .:-=+*#%@";
-
+    let simple_characters = " .:-=+*#%@";
+    let characters = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
     // This will range from 0 to 255 too, the higher the brighter the color is
     let brightness: f32 = 0.299 * red as f32 + 0.587 * green as f32 + 0.114 * blue as f32;
 
-    // Multiplying my 0.0392 (10/255) and rounding down, we will get a number between 0 and 9, inclusive. The lower this number is, the darkers the pixels rgb value was
+    // Multiplying my 0.0392 (10/255) and rounding down, we will get a number between 0 and 9, inclusive. The lower this number is, the darker the pixels rgb value was
     let char_index: usize = (0.0392 * brightness).floor() as usize;
-    characters.chars().nth(char_index).unwrap()
+
+    //let char_index: usize = (72 - (0.28235294118 * brightness).floor() as i32) as usize;
+    simple_characters.chars().nth(char_index).unwrap()
 }
 
 #[test]
 fn testing() {
     // White should be brightness (@) and black should be darkest ' '
-    assert_eq!(rgb_to_ascii(255, 255, 255), '@');
+    assert_eq!(rgb_to_ascii(255, 255, 255), '$');
     assert_eq!(rgb_to_ascii(0, 0, 0), ' ');
 }
